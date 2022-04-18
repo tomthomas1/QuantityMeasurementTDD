08a447ddb47a31f16fb8c0adb697f76a73af09e1
tomthomas1 <99910123+tomthomas1@users.noreply.github.com>
2022-04-18 16:14:34 +0530

    Compare Length

--- src/test/java/com/bridgelabz/QuantityTest.java
+++ src/test/java/com/bridgelabz/QuantityTest.java
@@ -3,12 +3,8 @@ package com.bridgelabz;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-
 import org.junit.Test;
-import org.mockito.Mockito;
-import org.mockito.junit.MockitoJUnit;
+
 
 public class QuantityTest {
 
